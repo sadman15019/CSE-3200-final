d42883ae0bfefe3aa90d0bcfd1cf717c482b28dd
humairaneha <humairaanzumn@gmail.com>
2022-12-24 02:08:33 +0600

    Startup page implemented

---
+++ app/src/main/java/com/app/mycamapp/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.app.mycamapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
